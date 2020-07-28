585e9ca24306515dc3edca16e9844df84dd6d3bc
jbdevelop <jbernardorj@gmail.com>
2020-07-27 21:14:34 -0300

    Build

---
+++ build/precache-manifest.a0744fe496a4fd72c794cf574c4f9b36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d6c8b306836bd10dd872b60867b7f76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9aa5664bca4c00c31877",
+    "url": "/static/css/main.97f687f9.chunk.css"
+  },
+  {
+    "revision": "bf794de3c1b9f50dfae5",
+    "url": "/static/js/2.963433c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.963433c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aa5664bca4c00c31877",
+    "url": "/static/js/main.4e68e537.chunk.js"
+  },
+  {
+    "revision": "23f7e4d2c8397ce55bf6",
+    "url": "/static/js/runtime-main.5fe610d3.js"
+  }
+]);
